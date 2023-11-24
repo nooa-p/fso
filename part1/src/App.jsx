ae7b5981a1c3f598d7fb274b3f8cd68ee7e359b5
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-24 11:37:37 +0200

    add multiple components to app.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,12 +1,16 @@
-const App = () => {
-  const now = new Date();
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-  
+const Hello = () => {
   return (
     <div>
       <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
     </div>
   )
 }
