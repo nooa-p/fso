036327ca3834ef79c2e8830330e4b3d5f748e155
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-28 10:05:05 +0200

    rerendering

--- lessons/part1/src/App.jsx
+++ lessons/part1/src/App.jsx
@@ -1,25 +1,8 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
 export default App
