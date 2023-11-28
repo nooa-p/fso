50b72bd8a2c9b684d1cb39dbc095100c9354ab44
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-28 12:51:34 +0200

    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,26 +14,57 @@ const Heading = ({ text }) => {
 
 const Static = ({ type, number }) => {
   return (
-    <p>{type} {number}</p>
+    <span>{type} {number}</span>
   )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [reviews, setReviews] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const handleGood = () => {
+    const newGood = reviews.good + 1
+    const newTotal = total + 1
+    setReviews({...reviews, good: newGood})
+    setTotal(newTotal)
+    setPositive((newGood / newTotal) * 100)
+    setAverage(((newGood) + (reviews.bad * -1)) / newTotal)
+  }
+
+  const handleNeutral = () => {
+    const newTotal = total + 1
+    setReviews({...reviews, neutral: reviews.neutral + 1})
+    setTotal(newTotal)
+    setPositive((reviews.good / (total + 1)) * 100)
+    setAverage(((reviews.good) + (reviews.bad * -1)) / newTotal)
+  }
+
+  const handleBad = () => {
+    const newTotal = total + 1
+    const newBad = reviews.bad + 1
+    setReviews({...reviews, bad: newBad})
+    setTotal(newTotal)
+    setPositive((reviews.good / newTotal) * 100)
+    setAverage((reviews.good + (newBad * -1)) / newTotal)
+  }
 
   return (
     <div>
       <Heading text='give feedback' />
-      <Button onClick={() => setGood(good + 1)} text={'good'} />
-      <Button onClick={() => setNeutral(neutral + 1)} text={'neutral'} />
-      <Button onClick={() => setBad(bad + 1)} text={'bad'} />
+      <Button onClick={handleGood} text={'good'} />
+      <Button onClick={handleNeutral} text={'neutral'} />
+      <Button onClick={handleBad} text={'bad'} />
       <Heading text='statistics' />
-      <Static type='good' number={good} />
-      <Static type='neutral' number={neutral} />
-      <Static type='bad' number={bad} />
+      <Static type='good' number={reviews.good} /> <br />
+      <Static type='neutral' number={reviews.neutral} /> <br />
+      <Static type='bad' number={reviews.bad} /> <br />
+      <Static type= 'total' number={total} /> <br />
+      <Static type='average' number={average} /> <br />
+      <Static type='positive' number={positive} /> %
     </div>
   )
 }
