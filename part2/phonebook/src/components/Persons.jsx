133c8de469c02d5179455c8e0ff92468c9f93e9d
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-29 13:13:43 +0200

    exercise 2.11

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -2,7 +2,7 @@ import Person from './Person'
 
 const Persons = (props) => {
     return (
-        props.names.map(person => <Person key={person.name} name={person.name} number={person.number} />)
+        props.names.map(person => <Person key={person.id} name={person.name} number={person.number} />)
     )
 }
 
