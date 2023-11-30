af0a5aa6e667eb407cd199a56e1b76f198d68322
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-30 09:27:45 +0200

    exerice 2.15

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -17,4 +17,9 @@ const deleteThis = (id) => {
     return request.then(response=> response.data)
 }
 
-export default { getAll, create, deleteThis }
+const changeNumber = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+export default { getAll, create, deleteThis, changeNumber }
