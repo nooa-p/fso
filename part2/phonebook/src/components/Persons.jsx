ecb455d938fbd8e95a8349e94cbf4ce5a1ac04e0
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-11-30 09:28:17 +0200

    fix typo

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -2,7 +2,7 @@ import Person from './Person'
 
 const Persons = (props) => {
     return (
-        props.names.map(person => <Person key={person.id} id={person.id} name={person.name} number={person.number} onClick={() => props.deleteName(person.id)} />)
+        props.names.map(person => <Person key={person.id} id={person.id} name={person.name} number={person.number} onSubmit={() => props.deleteName(person.id)} />)
     )
 }
 
