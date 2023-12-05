dc8e7755f01c9e008de3858afb262f43f91ba188
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-12-05 13:55:18 +0200

    return possibility to update numbers

--- part3/phonebook/src/App.jsx
+++ part3/phonebook/src/App.jsx
@@ -33,32 +33,28 @@ const App = () => {
       number: newNumber
     }
     if (persons.some((e) => e.name === newName)) {
-      setErrorMessage(`${newName} is already added to the phonebook.`)
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-      // if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
-      //   const person = persons.find(person => person.name === newName)
-      //   const changedPerson = {...person, number: newNumber}
-      //   nameService
-      //     .changeNumber(person.id, changedPerson)
-      //     .then(response => {
-      //       setPersons(persons.map(person => person.name !== newName ? person : response))
-      //       setAddedMessage(`Changed ${newName}`)
-      //       setTimeout(() => {
-      //         setAddedMessage(null)
-      //       }, 5000)
-      //       setNewName('')
-      //       setNewNumber('')
-      //     })
-      //     .catch(error => {
-      //       setErrorMessage(`Information of ${newName} has already been deleted from server`)
-      //       setTimeout(() => {
-      //         setErrorMessage(null)
-      //       }, 5000)
-      //       setPersons(persons.filter(person => person.id !== changedPerson.id))
-      //     })
-      // }
+      if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
+        const person = persons.find(person => person.name === newName)
+        const changedPerson = {...person, number: newNumber}
+        nameService
+          .changeNumber(person.id, changedPerson)
+          .then(response => {
+            setPersons(persons.map(person => person.name !== newName ? person : response))
+            setAddedMessage(`Changed ${newName}`)
+            setTimeout(() => {
+              setAddedMessage(null)
+            }, 5000)
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(error => {
+            setErrorMessage(`Information of ${newName} has already been deleted from server`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+            setPersons(persons.filter(person => person.id !== changedPerson.id))
+          })
+      }
     } else {
       nameService
         .create(nameObject)
