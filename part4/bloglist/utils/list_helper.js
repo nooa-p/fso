6853b2fdcc00555a7a88902d7b8ab41641aa144f
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-12-07 13:42:22 +0200

    exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,6 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  let total = 0;
+  blogs.forEach((blog) => {
+    total += blog.likes;
+  });
+  return total;
+};
+
 module.exports = {
-  dummy,
+  dummy, totalLikes,
 };
