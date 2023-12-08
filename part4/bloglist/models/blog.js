8926b3250b3890821c661dc60d55cecd31b95015
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-12-08 10:02:50 +0200

    exercise 4.9

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
@@ -5,6 +7,14 @@ const blogSchema = new mongoose.Schema({
   author: String,
   url: String,
   likes: Number,
+});
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -20,6 +20,11 @@ test('correct number of blogs is returned', async () => {
   expect(response.body).toHaveLength(3);
 });
 
+test('unique identifier of blog posts is id', async () => {
+  const response = await api.get('/api/blogs');
+  expect(response.body[0].id).toBeDefined();
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
