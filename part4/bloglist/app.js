4d19a607438b5724c4433b41490a1bac350ee229
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-12-13 09:46:09 +0200

    exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -14,6 +14,8 @@ const middleware = require('./utils/middleware');
 
 const logger = require('./utils/logger');
 
+const usersRouter = require('./controllers/users');
+
 mongoose.set('strictQuery', false);
 
 logger.info('connecting to', config.MONGODB_URI);
@@ -31,6 +33,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
