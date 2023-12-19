882479158ff244b2fc18d093d09d479dcd1a0f21
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2023-12-19 09:49:08 +0200

    exercise 5.1

--- part5/bloglist/src/App.jsx
+++ part5/bloglist/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [userName, setUserName] = useState('')
+  const [passWord, setPassWord] = useState('')
+  const [name, setName] = useState('') 
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +16,42 @@ const App = () => {
     )  
   }, [])
 
+  const nameChange = (e) => {
+    setUserName(e.target.value)
+  }
+
+  const passChange = (e) => {
+    setPassWord(e.target.value)
+  }
+
+  const logIn = (e) => {
+    e.preventDefault()
+    loginService
+    .login(userName, passWord)
+    .then(response => {
+      setUser(response.token)
+      setName(response.name)
+      setBlogs(blogs.filter((blog) => blog.user.username === userName))
+      })
+    }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>log in to application</h2>
+        <form onSubmit={logIn}>
+          username <input type="text" value={userName} onChange={nameChange} /> <br />
+          password <input type="password" value={passWord} onChange={passChange} /> <br />
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>{name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
